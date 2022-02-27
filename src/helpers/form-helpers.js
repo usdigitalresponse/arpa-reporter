77360c74fbf457da8924875472a8bcf04ae3a372
Andrew Hyndman <ajhyndman@hotmail.com>
2022-02-27 14:53:53 -0800

    Date form inputs accept anything moment can parse

--- src/helpers/form-helpers.js
+++ src/helpers/form-helpers.js
@@ -78,7 +78,7 @@ export function validate (columns, record) {
         }
       }
       if (value && column.date) {
-        const d = moment(value, 'MM/DD/YYYY')
+        const d = moment(value)
         if (!d.isValid()) {
           validationMessages.push(
             makeValidationMessage(column, 'must be a valid date')

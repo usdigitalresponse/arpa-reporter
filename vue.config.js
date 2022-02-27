3797deefcc046aadedbe5d85818e00f157b6b1ef
Andrew Hyndman <ajhyndman@hotmail.com>
2022-02-26 23:50:35 -0800

    Set websocket path to avoid proxying requests

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
-
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:3000'
+    proxy: 'http://localhost:3000',
+    client: {
+      // Set the websocket path to sockjs-node so that vue-cli-service knows
+      // not to proxy the request.
+      // https://github.com/vuejs/vue-cli/blob/master/packages/%40vue/cli-service/lib/util/prepareProxy.js#L53
+      webSocketURL: { pathname: '/sockjs-node' }
+    }
   }
 }

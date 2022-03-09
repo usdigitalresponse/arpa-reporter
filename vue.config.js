ec93fe1d5d6ed8791c7ee2975ea30cc7c360eaf8
Andrew Hyndman <ajhyndman@hotmail.com>
2022-03-08 20:32:29 -0800

    Instead of changing socket paths, restrict proxying to paths matching /api

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   devServer: {
-    proxy: 'http://localhost:3000',
-    client: {
-      // Set the websocket path to sockjs-node so that vue-cli-service knows
-      // not to proxy the request.
-      // https://github.com/vuejs/vue-cli/blob/master/packages/%40vue/cli-service/lib/util/prepareProxy.js#L53
-      webSocketURL: { pathname: '/sockjs-node' }
+    proxy: {
+      // proxy requests whose paths match the pattern below to express
+      '/api': {
+        target: 'http://localhost:3000'
+      }
     }
   }
 }

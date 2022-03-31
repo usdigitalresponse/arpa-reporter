ebbe63ceb91db83fadc0c0cf50463166fab08360
Igor Serebryany <igor47@moomers.org>
2022-03-31 04:30:36 -0700

    seeds: updated app settings
    create an env var for setting app name, or use a sensible name to
    prevent confusion/manual db editing

--- seeds/05_app_settings.js
+++ seeds/05_app_settings.js
@@ -1,8 +1,17 @@
 require('dotenv').config()
 
+const title = (process.env.INITIAL_APP_TITLE || 'ARPA Reporter')
+
 exports.seed = async function (knex) {
-  await knex('application_settings').del()
+  const [{ count }] = await knex('application_settings').count('title', { as: 'count' })
+  if (count !== '0') {
+    console.log('db already has application settings...')
+    return
+  }
+
+  const { firstPeriod } = await knex('reporting_periods').first('id AS firstPeriod')
+
   await knex('application_settings').insert([
-    { title: 'Rhode Island', current_reporting_period_id: 2 }
+    { title, current_reporting_period_id: firstPeriod }
   ])
 }

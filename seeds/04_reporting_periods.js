eb38b0bf21fc2f23cdfe796793760c5a3011948f
Igor Serebryany <igor47@moomers.org>
2022-03-31 04:29:54 -0700

    seeds: updated reporting periods
    based on the relevant upcoming periods from the treasury publications for ARPA

--- seeds/04_reporting_periods.js
+++ seeds/04_reporting_periods.js
@@ -1,73 +1,71 @@
 require('dotenv').config()
 
+/*
+   reporting periods loosely based on tables 3 and 4 from here:
+   https://home.treasury.gov/system/files/136/SLFRF-Compliance-and-Reporting-Guidance.pdf
+*/
 exports.seed = async function (knex) {
-  await knex('reporting_periods').del()
+  const [{ count }] = await knex('reporting_periods').count('name', { as: 'count' })
+  if (count !== '0') {
+    console.log(`db already has ${count} reporting periods...`)
+    return
+  }
+
   await knex('reporting_periods').insert([
     {
-      name: '1',
-      start_date: '2020-03-01',
-      end_date: '2020-09-30',
-      period_of_performance_end_date: '2020-12-30',
-      open_date: '2020-12-01',
-      close_date: '2020-12-01',
-      review_period_start_date: '2020-12-16',
-      review_period_end_date: '2020-12-23',
+      name: 'Quarterly 1',
+      start_date: '2021-03-03',
+      end_date: '2021-12-31',
+      period_of_performance_end_date: '2021-12-31',
+      open_date: '2022-01-01',
+      close_date: '2022-01-31',
+      review_period_start_date: '2022-01-16',
+      review_period_end_date: '2022-01-31',
       crf_end_date: '2021-09-30'
     },
     {
-      name: '2',
-      start_date: '2020-03-01',
-      end_date: '2020-09-30',
-      period_of_performance_end_date: '2020-12-30',
-      open_date: '2020-12-01',
-      close_date: '2020-12-01',
-      review_period_start_date: '2020-12-16',
-      review_period_end_date: '2020-12-23',
+      name: 'Quarterly 2',
+      start_date: '2022-01-01',
+      end_date: '2022-03-31',
+      period_of_performance_end_date: '2022-03-31',
+      open_date: '2022-04-01',
+      close_date: '2022-04-30',
+      review_period_start_date: '2022-04-16',
+      review_period_end_date: '2022-04-30',
       crf_end_date: '2021-09-30'
     },
     {
-      name: '3',
-      start_date: '2020-10-01',
-      end_date: '2020-12-31',
-      period_of_performance_end_date: '2020-12-30',
-      open_date: '2020-12-25',
-      close_date: '2021-01-11',
-      review_period_start_date: '2021-01-12',
-      review_period_end_date: '2021-01-20',
+      name: 'Quarterly 3',
+      start_date: '2022-04-01',
+      end_date: '2022-06-30',
+      period_of_performance_end_date: '2022-06-30',
+      open_date: '2022-07-01',
+      close_date: '2022-07-31',
+      review_period_start_date: '2022-07-16',
+      review_period_end_date: '2022-07-31',
       crf_end_date: '2021-09-30'
     },
     {
-      name: '4',
-      start_date: '2021-01-01',
-      end_date: '2021-03-31',
-      period_of_performance_end_date: '2020-12-30',
-      open_date: '2021-01-22',
-      close_date: '2021-04-12',
-      review_period_start_date: '2021-04-13',
-      review_period_end_date: '2021-04-20',
-      crf_end_date: '2021-09-30'
+      name: 'Annual 1',
+      start_date: '2021-03-03',
+      end_date: '2022-03-31',
+      period_of_performance_end_date: '2022-03-31',
+      open_date: '2022-04-01',
+      close_date: '2022-04-30',
+      review_period_start_date: '2022-04-16',
+      review_period_end_date: '2022-04-30',
+      crf_end_date: '2022-04-30'
     },
     {
-      name: '5',
-      start_date: '2021-04-01',
-      end_date: '2021-06-30',
-      period_of_performance_end_date: '2020-12-30',
-      open_date: '2021-04-22',
-      close_date: '2021-07-12',
-      review_period_start_date: '2021-07-13',
-      review_period_end_date: '2021-07-20',
-      crf_end_date: '2021-09-30'
-    },
-    {
-      name: '6',
-      start_date: '2021-07-01',
-      end_date: '2021-09-30',
-      period_of_performance_end_date: '2020-12-30',
-      open_date: '2021-07-22',
-      close_date: '2021-10-12',
-      review_period_start_date: '2021-10-13',
-      review_period_end_date: '2021-10-20',
-      crf_end_date: '2021-09-30'
+      name: 'Annual 2',
+      start_date: '2022-04-01',
+      end_date: '2023-03-31',
+      period_of_performance_end_date: '2023-03-31',
+      open_date: '2023-04-01',
+      close_date: '2023-04-30',
+      review_period_start_date: '2023-04-16',
+      review_period_end_date: '2023-04-30',
+      crf_end_date: '2023-04-30'
     }
   ])
     .returning('id')

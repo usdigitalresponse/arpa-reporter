690a0ac6023091a5b6193cd4084338c9a3838f2b
Igor Serebryany <igor47@moomers.org>
2022-04-13 18:32:24 -0700

    test: smarter modification of test db params
    allows running tests with a local sql db with passwords/out of docker

--- tests/server/mocha_wrapper.sh
+++ tests/server/mocha_wrapper.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+
+set +errexit
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export POSTGRES_URL="postgres://postgres@localhost/server_test"
+
+# Import .env variables if not already defined.
+DOTENV="$DIR/../../.env"
+source /dev/stdin <<DONE
+$(grep -v '^#' $DOTENV | sed -E 's|^(.+)=(.*)|: ${\1=\2}; export \1|g')
+DONE
+
+export DEVDBNAME="${POSTGRES_URL##*/}"
+export POSTGRES_URL="${POSTGRES_URL}_test"
 export UPLOAD_DIRECTORY=`dirname $0`/mocha_uploads
 export TREASURY_DIRECTORY=`dirname $0`/mocha_uploads/treasury
 
-$DIR/reset-db.sh server_test
+$DIR/reset-db.sh
 
 if [ $# -gt 0 ]; then
   mocha --require=`dirname $0`/mocha_init.js $*
--- tests/server/reset-db.sh
+++ tests/server/reset-db.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
 set -eo pipefail
 
-# The actual directory of this file.
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# set -x
-set -e
+dbconn=${POSTGRES_URL#*//}  # from postgres://user:pass@host/dbname -> user:pass@host/dbname
+userpass=${dbconn%@*}       # 'user:pass'
+hostdbname=${dbconn#*@}         # host/dbname
 
-# Import .env variables if not already defined.
-DOTENV="$DIR/../../.env"
-source /dev/stdin <<DONE
-$(grep -v '^#' $DOTENV | sed -E 's|^(.+)=(.*)|: ${\1=\2}; export \1|g')
-DONE
+username=${userpass%:*}
+password=${userpass#*:}
 
-# Default dbname is "postgres"
-dbname=${1:-postgres}
+host=${hostdbname%/*}
+dbname=${hostdbname#*/}
 
 echo Using database $dbname
 
 mkdir -p $UPLOAD_DIRECTORY
 rm -rf $UPLOAD_DIRECTORY/*
 
-if [[ $dbname = postgres ]]
-then
-  # if the dbname is postgres it's easier to drop the schema than the database.
-  psql -h localhost -U postgres -w postgres -c "DROP SCHEMA public CASCADE"
-  psql -h localhost -U postgres -w postgres -c "CREATE SCHEMA public"
-else
-
-  psql -h localhost -U postgres -w postgres -c "DROP DATABASE IF EXISTS $dbname"
-  psql -h localhost -U postgres -w postgres -c "CREATE DATABASE $dbname"
-fi
+PGPASSWORD=$password psql -h $host -U $username -w ${DEVDBNAME} -c "DROP DATABASE IF EXISTS $dbname"
+PGPASSWORD=$password psql -h localhost -U postgres -w ${DEVDBNAME} -c "CREATE DATABASE $dbname"
 
 yarn knex migrate:latest
 yarn knex --knexfile tests/server/knexfile.js seed:run

2dcab72ac88a1f77501c89ba939d7a3501beccde
Igor Serebryany <igor47@moomers.org>
2022-04-21 18:54:01 -0700

    uploads: simplify upload creation
    we can use simple knex and avoid all the on-conflict logic. we add
    tests!

--- src/server/db/uploads.js
+++ src/server/db/uploads.js
@@ -58,35 +58,12 @@ function getUploadSummaries (period_id) {
 }
 
 async function createUpload (upload, queryBuilder = knex) {
-  // The CONFLICT should never happen, because the file upload should stop
-  // if there is an existing `filename` in the upload directory.
-  // However if `filename` gets deleted for some reason without deleting
-  // the DB record, it's better to update this record than mysteriously fail.
+  const inserted = await queryBuilder('uploads')
+    .insert(upload)
+    .returning('*')
+    .then(rows => rows[0])
 
-  const timestamp = new Date().toISOString()
-  const qResult = await queryBuilder.raw(
-    `INSERT INTO uploads
-      (created_by, filename, user_id, created_at, agency_id, project_id, reporting_period_id)
-      VALUES
-      (:created_by, :filename, :user_id, '${timestamp}', :agency_id, :project_id, :reporting_period_id)
-      ON CONFLICT (filename) DO UPDATE
-        SET
-          created_by = :created_by,
-          filename = :filename,
-          user_id = :user_id,
-          created_at = '${timestamp}',
-          agency_id = :agency_id,
-          project_id = :project_id,
-          reporting_period_id = :reporting_period_id
-      RETURNING "id", "created_at"`,
-    upload
-  )
-  const inserted = _.get(qResult, 'rows[0]')
-  // This should also never happen, but better to know if it does.
-  if (!inserted) throw new Error('Unknown error inserting into uploads table')
-  upload.id = inserted.id
-  upload.created_at = inserted.created_at
-  return upload
+  return inserted
 }
 
 async function getPeriodUploadIDs (period_id) {
---
+++ tests/server/db/uploads.spec.js
@@ -0,0 +1,46 @@
+
+const uploads = requireSrc(__filename)
+const assert = require('assert')
+const { knex } = require('../mocha_init')
+
+describe('db/uploads.js', function () {
+  let upload
+
+  beforeEach('init an upload row', async function () {
+    upload = {
+      filename: 'filename',
+      created_by: 'user@email.address'
+    }
+  })
+
+  describe('createUpload', function () {
+    afterEach('clear uploads table', async function () {
+      await knex.raw('TRUNCATE TABLE uploads CASCADE')
+    })
+
+    it('Returns the resulting row', async function () {
+      const inserted = await uploads.createUpload(upload)
+      assert.ok(inserted)
+      assert.equal(inserted.filename, 'filename')
+    })
+
+    it('Requires a filename', async function () {
+      upload.filename = null
+      assert.rejects(async () => await uploads.createUpload(upload))
+    })
+
+    describe('when there is invalid user id', function () {
+      it('throws an error', async function () {
+        upload.user_id = 12345
+        assert.rejects(async () => await uploads.createUpload(upload))
+      })
+    })
+
+    describe('when there is invalid reporting period', function () {
+      it('throws an error', async function () {
+        upload.reporting_period_id = 12345
+        assert.rejects(async () => await uploads.createUpload(upload))
+      })
+    })
+  })
+})

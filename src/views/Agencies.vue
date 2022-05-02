a324aae5dd9b036e18500307ae3b3cd790798ab3
Igor Serebryany <igor47@moomers.org>
2022-05-01 20:15:51 -0700

    agencies: use normal <table>, not <DataTable>
    oh man the code is so much simpler. we delete the AgencyLink component
    which is no longer necessary, the link is now in-line in the agency
    table

--- src/views/Agencies.vue
+++ src/views/Agencies.vue
@@ -1,39 +1,42 @@
 <template>
   <div class="data">
     <h2>Agencies</h2>
+
     <div class="mb-4">
-      <router-link to="/new_agency" class="btn btn-primary"
-        >Create New Agency</router-link
-      >
+      <router-link to="/new_agency" class="btn btn-primary">
+        Create New Agency
+      </router-link>
     </div>
+
     <div>
-      <DataTable v-if="agencies" :table="table" :rows="agencies" :user="user" />
+      <table v-if="agencies" class="table table-striped">
+        <thead>
+          <tr>
+            <th>Agency Code</th>
+            <th>Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="agency in agencies" :key="agency.code">
+            <td>{{ agency.code }}</td>
+            <td>{{ agency.name }}</td>
+            <td>
+              <router-link :to="`/agencies/${agency.id}`" class="btn btn-primary">
+                Edit
+              </router-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
 
 <script>
-import DataTable from '../components/DataTable'
-import AgencyLink from '../components/AgencyLink'
 export default {
   name: 'Agencies',
-  components: {
-    DataTable
-  },
-  data: function () {
-    const user = this.$store.state.user
-    return {
-      user,
-      table: {
-        views: [],
-        columns: [
-          { name: 'code', label: 'Agency Code' },
-          { name: 'name' },
-          { component: AgencyLink }
-        ]
-      }
-    }
-  },
   computed: {
     agencies: function () {
       return this.$store.state.agencies
@@ -41,6 +44,7 @@ export default {
   }
 }
 </script>
+
 <style scoped>
 .data {
   width: 90%;

3085c2aef50e89acfd3a89e0cb540dab93b08c9d
Andrew Hyndman <ahyndman@dropbox.com>
2022-05-16 18:32:56 -0700

    Replace UPLOAD_DIRECTORY with generalizable DATA_DIR

--- src/server/environment.js
+++ src/server/environment.js
@@ -1,9 +1,9 @@
-
 const { join, resolve } = require('path')
 
 const VERBOSE = Boolean(process.env.VERBOSE)
 
-const UPLOAD_DIR = resolve(process.env.UPLOAD_DIRECTORY)
+const DATA_DIR = resolve(process.env.DATA_DIR)
+const UPLOAD_DIR = join(DATA_DIR, 'uploads')
 
 const SRC_DIR = resolve(join(__dirname, '..'))
 const SERVER_DATA_DIR = join(SRC_DIR, 'server', 'data')
@@ -11,6 +11,7 @@ const SERVER_DATA_DIR = join(SRC_DIR, 'server', 'data')
 const EMPTY_TEMPLATE_NAME = 'ARPA SFRF Reporting Workbook v20220419.xlsm'
 
 module.exports = {
+  DATA_DIR,
   UPLOAD_DIR,
   SRC_DIR,
   SERVER_DATA_DIR,

ede79eca908e71cbc941d6ee71f71b05ab613322
Igor Serebryany <igor47@moomers.org>
2022-05-18 15:52:52 -0700

    Update ARPA template to latest version (20220516)

--- src/server/environment.js
+++ src/server/environment.js
@@ -16,7 +16,7 @@ const ARPA_REPORTS_DIR = join(DATA_DIR, 'arpa_reports')
 const SRC_DIR = resolve(join(__dirname, '..'))
 const SERVER_DATA_DIR = join(SRC_DIR, 'server', 'data')
 
-const EMPTY_TEMPLATE_NAME = 'ARPA SFRF Reporting Workbook v20220419.xlsm'
+const EMPTY_TEMPLATE_NAME = 'ARPA SFRF Reporting Workbook v20220516.xlsm'
 
 module.exports = {
   DATA_DIR,

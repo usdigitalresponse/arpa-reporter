363fec27c56a98945caadc7ab41f4b648b7a0fdf
Matt Broussard <mbroussard@usdigitalresponse.org>
2022-05-19 17:59:31 -0700

    chore: update UNIQUE constraints for projects table

---
+++ migrations/20220519182830_multitenant_unique_constraints.js
@@ -0,0 +1,50 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  // In order to drop the UNIQUE constraint, we have to first drop FOREIGN KEY
+  // references to it (then re-add a composite version after), otherwise Postgres
+  // errors with "cannot drop constraint projects_code_unique on table projects
+  // because other objects depend on it"
+  // See https://dba.stackexchange.com/q/137240
+
+  // projects table
+  let schema = knex.schema
+    .alterTable("period_summaries", function (table) {
+      table.dropForeign(["project_code"]);
+    })
+    .alterTable("projects", function (table) {
+      table.unique(["tenant_id", "code"]).dropUnique(["code"]);
+      table.unique(["tenant_id", "name"]).dropUnique(["name"]);
+    })
+    .alterTable("period_summaries", function (table) {
+      table
+        .foreign(["tenant_id", "project_code"])
+        .references(["tenant_id", "code"])
+        .inTable("projects");
+    });
+
+  return schema;
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  // projects table
+  let schema = knex.schema
+    .alterTable("period_summaries", function (table) {
+      table.dropForeign(["tenant_id", "project_code"]);
+    })
+    .alterTable("projects", function (table) {
+      table.unique(["code"]).dropUnique(["tenant_id", "code"]);
+      table.unique(["name"]).dropUnique(["tenant_id", "name"]);
+    })
+    .alterTable("period_summaries", function (table) {
+      table.foreign("project_code").references("projects.code");
+    });
+
+  return schema;
+};

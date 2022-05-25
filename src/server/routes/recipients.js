07f033a6c6b07c0daa04b8b2985888026e47a260
Igor Serebryany <igor47@moomers.org>
2022-05-25 16:40:16 -0700

    recipients: actually persist updates

--- src/server/routes/recipients.js
+++ src/server/routes/recipients.js
@@ -4,7 +4,8 @@ const express = require('express')
 const router = express.Router()
 const { requireUser } = require('../access-helpers')
 
-const { listRecipients, getRecipient } = require('../db/arpa-recipients')
+const { user: getUser } = require('../db/users')
+const { listRecipients, getRecipient, updateRecipient } = require('../db/arpa-recipients')
 const { rulesForPeriod } = require('../services/validation-rules')
 
 router.get('/', requireUser, async function (req, res) {
@@ -24,6 +25,27 @@ router.get('/:id', requireUser, async (req, res) => {
 
   const rules = await rulesForPeriod(recipient.reporting_period_id)
   res.json({ recipient, rules: rules.subrecipient })
+})
+
+router.post('/:id', requireUser, async (req, res) => {
+  const id = Number(req.params.id)
+  const user = await getUser(req.signedCookies.userId)
+
+  const recipient = await getRecipient(id)
+  if (!recipient) {
+    res.sendStatus(404)
+    res.end()
+    return
+  }
+
+  const record = JSON.parse(req.body.record)
+  if (record.Unique_Entity_Identifier__c !== recipient.uei || record.EIN__c !== recipient.tin) {
+    res.status(400).json({ error: 'Record cannot modify UEI or TIN' })
+    return
+  }
+
+  const updatedRecipient = await updateRecipient(recipient.id, { record, updatedByUser: user })
+  res.json({ recipient: updatedRecipient })
 })
 
 module.exports = router
--- src/views/Recipient.vue
+++ src/views/Recipient.vue
@@ -16,8 +16,20 @@
           <router-link :to="`/uploads/${recipient.upload_id}`">
             Upload {{ recipient.upload_id }}
           </router-link>
-          on {{ createdAtStr }}
+          on {{ humanDate(recipient.created_at) }}
           by {{ recipient.created_by }}
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <div class="col-sm-2">
+          Updated:
+        </div>
+        <div class="col-sm-10">
+          <span v-if="!recipient.updated_at">Never manually updated</span>
+          <span v-else>
+            By {{ recipient.updated_by_email }} on {{ humanDate(recipient.updated_at) }}
+          </span>
         </div>
       </div>
 
@@ -25,7 +37,8 @@
         <label :for="key" class="col-sm-2 col-form-label">{{ rule.humanColName }}</label>
         <div class="col-sm-10">
           <select v-if="rule.listVals.length > 0" :id="key" v-model="record[key]" :readonly="isReadOnly(key)">
-            <option v-for="opt in rule.listVals" :key="opt">{{ opt }}</option>
+            <option :value="null"></option>
+            <option v-for="opt in rule.listVals" :key="opt" :value="opt">{{ opt }}</option>
           </select>
 
           <input v-else type="text" class="form-control" :id="key" v-model="record[key]" :readonly="isReadOnly(key)">
@@ -34,7 +47,7 @@
 
       <div class="form-group row">
         <div class="col-sm-2">
-          <button class="btn btn-primary">Save</button>
+          <button class="btn btn-primary" v-on:click="updateRecipient" :disabled="saving">Save</button>
         </div>
 
         <div class="col-sm-2">
@@ -47,7 +60,7 @@
 
 <script>
 import moment from 'moment'
-import { getJson } from '../store'
+import { getJson, post } from '../store'
 
 export default {
   name: 'Recipient',
@@ -55,7 +68,8 @@ export default {
     return {
       recipient: null,
       rules: [],
-      record: {}
+      record: {},
+      saving: false
     }
   },
   computed: {
@@ -82,11 +96,39 @@ export default {
         this.setRecord()
       }
     },
+    updateRecipient: async function () {
+      this.saving = true
+
+      try {
+        const record = JSON.stringify(Object.fromEntries(
+          Object.entries(this.record).filter(([key, val]) => val !== null)
+        ))
+
+        const result = await post(`/api/recipients/${this.recipientId}`, { record })
+        if (result.error) throw new Error(result.error)
+
+        this.$store.commit('addAlert', {
+          text: `Recipient ${this.recipientId} successfully updated`,
+          level: 'ok'
+        })
+      } catch (err) {
+        this.$store.commit('addAlert', {
+          text: `Error updating recipient ${this.recipientId}: ${err.message}`,
+          level: 'err'
+        })
+      }
+
+      this.saving = false
+      this.loadRecipient()
+    },
     setRecord: function () {
       this.record = this.recipient ? JSON.parse(this.recipient.record) : {}
     },
     isReadOnly: function (key) {
       return key === 'Unique_Entity_Identifier__c' || key === 'EIN__c'
+    },
+    humanDate: function (date) {
+      return date && moment(date).local().format('MMM Do YYYY, h:mm:ss A')
     }
   },
   mounted: async function () {

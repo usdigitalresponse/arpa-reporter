fa7c5a7f318ea6e07d1e004a8de32883845928c6
Andrew Hyndman <ajhyndman@hotmail.com>
2022-05-27 16:51:09 -0700

    generateProject31: populate rows

--- src/server/services/generate-arpa-report.js
+++ src/server/services/generate-arpa-report.js
@@ -300,8 +300,30 @@ async function generateProject31 (records) {
       const detailedEcCode = getDetailedEcCode(record)
       switch (detailedEcCode) {
         case '3.1': {
-          // TODO
-          return null
+          return [
+            null, // first col is blank
+            record.type, // FIXME: transform from sheet tab to export format
+            record.subcategory,
+            record.content.Name,
+            record.content.Project_Identification_Number__c,
+            record.content.Completion_Status__c,
+            record.content.Adopted_Budget__c,
+            record.content.Total_Obligations__c,
+            record.content.Total_Expenditures__c,
+            record.content.Current_Period_Obligations__c,
+            record.content.Current_Period_Expenditures__c,
+            record.content.Does_Project_Include_Capital_Expenditure__c,
+            record.content.Total_Cost_Capital_Expenditure__c,
+            record.content.Type_of_Capital_Expenditure__c,
+            record.content.Type_of_Capital_Expenditure_Other__c,
+            record.content.Capital_Expenditure_Justification__c,
+            record.content.Project_Description__c,
+            record.content.Program_Income_Earned__c,
+            record.content.Program_Income_Expended__c,
+            record.content.Structure_Objectives_of_Asst_Programs__c,
+            record.content.Recipient_Approach_Description__c,
+            record.content.Payroll_Public_Health_Safety__c
+          ]
         }
         default:
           return null

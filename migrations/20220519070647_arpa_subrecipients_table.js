8fa17e640675773a04fa03be4702b9aab381f833
Andrew Hyndman <ajhyndman@hotmail.com>
2022-05-27 15:34:37 -0700

    db migration: add tenant_id to arpa_subrecipients

--- migrations/20220519070647_arpa_subrecipients_table.js
+++ migrations/20220519070647_arpa_subrecipients_table.js
@@ -2,13 +2,17 @@ exports.up = function (knex) {
   return knex.schema
     .createTable('arpa_subrecipients', function (table) {
       table.increments('id').primary()
+      table.integer('tenant_id').notNullable().defaultTo(0)
       table.timestamp('created_at').notNullable().defaultTo(knex.fn.now())
       table.timestamp('updated_at')
       table.integer('updated_by').unsigned()
-      table.string('uei').unique()
-      table.string('tin').unique()
+      table.string('uei')
+      table.string('tin')
       table.text('record')
       table.integer('upload_id').unsigned()
+
+      table.unique(['tenant_id', 'uei'])
+      table.unique(['tenant_id', 'tin'])
 
       table.foreign('updated_by').references('users.id')
       table.foreign('upload_id').references('uploads.id')

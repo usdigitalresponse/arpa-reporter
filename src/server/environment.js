872aa5644171ea23f3adb823c62f19aa0392803a
Andrew Hyndman <ajhyndman@hotmail.com>
2022-06-16 19:21:43 -0700

    Define API_DOMAIN and WEBSITE_DOMAIN env vars

--- src/server/environment.js
+++ src/server/environment.js
@@ -26,7 +26,9 @@ const IS_DEV = NODE_ENV === 'development'
 
 const LOGIN_EXPIRY_MINUTES = parseInt(process.env.LOGIN_EXPIRY_MINUTES) || 30
 
-const PORT = process.env.PORT || 3000
+const PORT = parseInt(process.env.PORT) || 3000
+const API_DOMAIN = process.env.API_DOMAIN
+const WEBSITE_DOMAIN = process.env.WEBSITE_DOMAIN
 
 const NOTIFICATIONS_EMAIL = process.env.NOTIFICATIONS_EMAIL
 const NOTIFICATIONS_EMAIL_PW = process.env.NOTIFICATIONS_EMAIL_PW
@@ -51,6 +53,8 @@ module.exports = {
   IS_DEV,
   LOGIN_EXPIRY_MINUTES,
   PORT,
+  API_DOMAIN,
+  WEBSITE_DOMAIN,
   NOTIFICATIONS_EMAIL,
   NOTIFICATIONS_EMAIL_PW,
   NODEMAILER_HOST,

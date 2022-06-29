71d0bbd44f98011f20a59975429e6c66b848489a
Andrew Hyndman <ajhyndman@hotmail.com>
2022-06-28 20:05:36 -0400

    Rewrite cleandb script

---
+++ scripts/cleandb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Load .env file if present
+set -a; source .env; set +a
+# abort on exception
+set =e
+
+# Get DB name from connection URI
+dbname=${POSTGRES_URL#*//*/}
+# Remove DB name from connection URI
+postgres_url=${POSTGRES_URL/$dbname}
+
+echo $dbname
+
+psql $postgres_url -c "DROP SCHEMA public CASCADE"
+psql $postgres_url -c "CREATE SCHEMA public"
+psql $postgres_url -c "DROP DATABASE IF EXISTS $dbname"
+psql $postgres_url -c "CREATE DATABASE $dbname"

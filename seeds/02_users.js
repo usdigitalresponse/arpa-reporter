e9dcf67a35e384bfac13d4b34d13d1a957d25e5d
Truman Cranor <trumancranor@gmail.com>
2022-07-04 14:36:42 -0700

    Add new admins to the users seed file

--- seeds/02_users.js
+++ seeds/02_users.js
@@ -3,7 +3,9 @@ require('dotenv').config()
 const usdrAdmins = [
   { email: 'igor47@moomers.org', name: 'Igor Serebryany', tenant_id: 0 },
   { email: 'ajhyndman@hotmail.com', name: 'Andrew Hyndman', tenant_id: 0 },
-  { email: 'joecomeau01@gmail.com', name: 'Joe Comeau', tenant_id: 0 }
+  { email: 'joecomeau01@gmail.com', name: 'Joe Comeau', tenant_id: 0 },
+  { email: 'trumancranor@gmail.com', name: 'Truman Cranor', tenant_id: 0 },
+  { email: 'mhuang@usdigitalresponse.org', name: 'Mindy Huang', tenant_id: 0 }
 ]
 
 exports.seed = async function (knex) {

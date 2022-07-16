42ff7bebc031aa4fb8deaa6f07968ea3552e0326
Andrew Hyndman <ajhyndman@hotmail.com>
2022-07-16 17:26:10 -0400

    Knex should return the raw date string for date fields

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -1,6 +1,12 @@
+const { types } = require('pg')
 const { POSTGRES_URL } = require('../environment')
 
 console.log('\nConnecting to database:', POSTGRES_URL)
+
+// override parser for date fields — just return the raw string content
+const DATE_OID = 1082
+types.setTypeParser(DATE_OID, value => value)
+
 const knex = require('knex')({
   client: 'pg',
   connection: POSTGRES_URL,

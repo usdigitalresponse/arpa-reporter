fa7b0810918b27694b8a09ba241205bc262ce653
Andrew Hyndman <ajhyndman@hotmail.com>
2022-07-23 22:03:40 -0400

    fixup! Update test assertions

--- tests/server/services/generate-arpa-report.spec.js
+++ tests/server/services/generate-arpa-report.spec.js
@@ -1,11 +1,12 @@
 const assert = require('assert')
 
-const { generateReport } = require(__filename)
+const { generateReport } = require('../../../src/server/services/generate-arpa-report')
 const { withTenantId } = require('../helpers/with-tenant-id')
 
 describe('arpa report generation', function () {
   it('generates a report', async function () {
-    const report = await withTenantId(1, generateReport)
+    const tenantId = 0
+    const report = await withTenantId(tenantId, () => generateReport(1))
     assert.ok(report)
   })
 })

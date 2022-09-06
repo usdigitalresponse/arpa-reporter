7552a259c9b4e0f4d1ca9c876fdc343cd49f2352
Matt Broussard <mbroussard@usdigitalresponse.org>
2022-09-06 11:17:04 -0700

    chore: fix environment test to not depend on dynamic import path

--- tests/server/environment.spec.js
+++ tests/server/environment.spec.js
@@ -1,14 +1,40 @@
 const assert = require('assert')
+const path = require('path')
+const fs = require('fs/promises')
 
-const underTest = '../../src/server/environment'
+const env = require('../../src/server/environment')
 
 describe('environment settings', function () {
-  beforeEach('clear env module', function () {
-    delete require.cache[require.resolve(underTest)]
+  it('SERVER_CODE_DIR point to the right place', async function () {
+    const testPaths = [
+      'environment.js',
+      'lib/ensure-async-context.js'
+    ]
+
+    for (const relative of testPaths) {
+      const absolute = path.join(env.SERVER_CODE_DIR, relative)
+      const pathExists = await exists(absolute)
+      assert.ok(pathExists, `expected ${absolute} to exist, is SERVER_CODE_DIR wrong?`)
+    }
   })
 
-  it('points the CODE_DIR at src', function () {
-    const env = require(underTest)
-    assert.equal(env.SRC_DIR.slice(env.SRC_DIR.length - 3), 'src')
+  it('SERVER_DATA_DIR point to the right place', async function () {
+    const testPaths = [
+      'treasury',
+      env.EMPTY_TEMPLATE_NAME
+    ]
+
+    for (const relative of testPaths) {
+      const absolute = path.join(env.SERVER_DATA_DIR, relative)
+      const pathExists = await exists(absolute)
+      assert.ok(pathExists, `expected ${absolute} to exist, is SERVER_DATA_DIR wrong?`)
+    }
   })
 })
+
+function exists (fpath) {
+  return fs.access(fpath).then(
+    () => true,
+    () => false
+  )
+}

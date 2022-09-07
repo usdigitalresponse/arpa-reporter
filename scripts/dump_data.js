a85f5d9a28a386048126ddde42e23b9cb21007f6
Matt Broussard <mbroussard@usdigitalresponse.org>
2022-09-06 23:21:23 -0700

    chore: add missedFiles check, debug output

--- scripts/dump_data.js
+++ scripts/dump_data.js
@@ -3,10 +3,12 @@ const AdmZip = require("adm-zip");
 const fs = require("fs/promises");
 const inquirer = require("inquirer");
 const path = require("path");
+const { list: listFiles } = require("recursive-readdir-async");
 
 const knex = require("../src/server/db/connection");
 const { uploadFSName } = require("../src/server/services/persist-upload");
 const { periodTemplatePath } = require("../src/server/services/get-template");
+const { UPLOAD_DIR } = require("../src/server/environment");
 
 const TABLES = [
   "users",
@@ -34,7 +36,7 @@ async function getDatabaseContents() {
   return ret;
 }
 
-function getAllFilePaths(dbContents) {
+function getAllFilesToCopy(dbContents) {
   const { uploads, reporting_periods } = dbContents;
 
   return [
@@ -45,47 +47,63 @@ function getAllFilePaths(dbContents) {
   ];
 }
 
-function addFilesToZip(dbContents, zipFile) {
-  const filePaths = getAllFilePaths(dbContents);
-  console.log('Adding', filePaths.length, 'uploaded files to zip output...');
+async function getAllFilesInUploadDir() {
+  const files = await listFiles(UPLOAD_DIR, {
+    recursive: true,
+    ignoreFolders: true,
+  });
+
+  return files.map((f) => f.fullname);
+}
+
+async function addFilesToZip(dbContents, zipFile) {
+  const pathsToCopy = getAllFilesToCopy(dbContents);
+  console.log("Adding", pathsToCopy.length, "uploaded files to zip output...");
   const start = Date.now();
 
   // Sanity check: all filenames should be unique since they will all be together
   // in the zip
-  const fnames = filePaths.map((filePath) => path.basename(filePath));
+  const fnames = pathsToCopy.map((filePath) => path.basename(filePath));
   const dupeFilenames = _.chain(fnames)
     .groupBy(_.identity)
     .pickBy((v) => v.length > 1)
     .keys()
     .value();
   if (dupeFilenames.length > 0) {
-    console.error("Duplicate filenames:", dupeFilenames);
-    throw new Error("duplicate filenames cannot be added to zip!");
+    console.error("ERROR: duplicate filenames:", dupeFilenames);
   }
 
-  for (const filePath of filePaths) {
-    zipFile.addLocalFile(filePath, path.join('files', path.basename(filePath)));
+  for (const filePath of pathsToCopy) {
+    zipFile.addLocalFile(filePath, path.join("files", path.basename(filePath)));
   }
 
   // Sanity check: look for files in upload directory not captured by the above
   // TODO
+  const allUploadPaths = await getAllFilesInUploadDir();
+  const missedFiles = _.difference(allUploadPaths, pathsToCopy);
+  if (missedFiles.length) {
+    console.warn("WARN: some files in upload dir not captured:", missedFiles);
+  }
 
   const end = Date.now();
-  console.log('Took', end-start, 'ms to add uploads to zip');
+  console.log("Took", end - start, "ms to add uploads to zip");
+
+  return { pathsToCopy, dupeFilenames, missedFiles };
 }
 
 function writeZip(zipFile, outputFilename) {
-  console.log('Writing zip file...');
+  console.log("Writing zip file...");
   const start = Date.now();
 
   zipFile.writeZip(outputFilename);
 
   const end = Date.now();
-  console.log('Took', end-start, 'ms to write zip file');
+  console.log("Took", end - start, "ms to write zip file");
 }
 
 async function main() {
-  const dateStr = new Date()
+  const runDate = new Date();
+  const dateStr = runDate
     .toISOString()
     .replace(/[^0-9]/g, "")
     .substring(0, 14);
@@ -102,10 +120,25 @@ async function main() {
 
   // Get db contents
   const dbContents = await getDatabaseContents();
-  zipFile.addFile("sql.json", Buffer.from(JSON.stringify(dbContents), "utf8"));
+  zipFile.addFile(
+    "sql.json",
+    Buffer.from(JSON.stringify(dbContents, undefined, 2), "utf8")
+  );
 
   // Copy files to zip
-  addFilesToZip(dbContents, zipFile);
+  const addFilesDebugInfo = await addFilesToZip(dbContents, zipFile);
+
+  // Collect some debug information
+  const debug = {
+    runDate,
+    outputPath: outputFilename,
+    numRows: _.mapValues(dbContents, (v) => v.length),
+    ...addFilesDebugInfo,
+  };
+  zipFile.addFile(
+    "debug.json",
+    Buffer.from(JSON.stringify(debug, undefined, 2), "utf8")
+  );
 
   // Generate the zip file
   writeZip(zipFile, outputFilename);
